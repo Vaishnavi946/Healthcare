d20b154f094d7ece76fcc0b83e7617b47432a897
Vaishnavi946 <mohitevaishnavi953@gmail.com>
2026-03-31 11:29:00 +0530

    fix port for render

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,20 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-const doctorRoutes = require("./routes/doctorRoutes");
 
 app.use("/api/doctor", require("./routes/doctorRoutes"));
 app.use("/api/appointment", require("./routes/appointmentRoutes"));
 app.use("/api/patient", require("./routes/patientRoutes"));
 
-app.get("/", (req, res) => res.send("API running"));
+app.get("/", (req, res) => {
+  res.send("API running");
+});
 
-app.listen(4000, () => console.log("Server running on 4000"));
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
